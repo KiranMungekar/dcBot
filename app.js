838fb4189d7a2a14cf4693df5fbb76b43f98d334
Kiran <mungekar.kiran30@gmail.com>
2020-10-17 18:59:14 +0530

    Kick member added

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ botClient.once('ready',async ()=>{
 });
 
 botClient.on('message', async (message)=>{
-    console.log(message.content);
+    //console.log(message.member);
+
+    //
+    if(message.content.includes(PREFIX) && message.content.includes("kick") && message.member.hasPermission(['KICK_MEMBERS']) ){
+        //console.log(message.content);
+        const member= message.mentions.members.first();
+        console.log(message.member.kickable)
+        try{
+            let kickResponse= await member.kick();
+           
+            message.channel.send(":wave: "+ kickResponse.displayName +", Thank you for using our channel Good Bye"); 
+        }catch(err){
+            message.channel.send(":wave: "+ message.author.username +", Boi you dont have permission to kick!! "+err.message); 
+        }
+          
+    }
 })
 
 
